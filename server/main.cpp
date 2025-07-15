fd3d90ed97931ebf07933b8cab3a7ae0b10047b8
Marcia Pedals <marcia.pedals@gmail.com>
2025-07-15 15:53:19 -0700

    serve stops

--- server/main.cpp
+++ server/main.cpp
@@ -1,11 +1,45 @@
+#include <iostream>
+
 #include "crow.h"
+#include "src/gtfs/gtfs.h"
 
 int main() {
-    crow::SimpleApp app;
+  crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world oh yeah!";
-    });
+  std::vector<vats5::Stop> stops;
+  try {
+    stops = vats5::GtfsLoadStops("../data/RG/stops.txt");
+    std::cout << "Loaded " << stops.size() << " stops from GTFS data"
+              << std::endl;
+  } catch (const std::exception& e) {
+    std::cerr << "Error loading GTFS data: " << e.what() << std::endl;
+    return 1;
+  }
 
-    app.port(18080).run();
-} 
+  CROW_ROUTE(app, "/")([]() { return "Hello world oh yeah!"; });
+
+  CROW_ROUTE(app, "/stops")([&stops]() {
+    crow::json::wvalue json_stops = crow::json::wvalue::list();
+
+    for (size_t i = 0; i < stops.size(); ++i) {
+      const auto& stop = stops[i];
+      crow::json::wvalue stop_json;
+      stop_json["stop_id"] = stop.stop_id;
+      stop_json["stop_name"] = stop.stop_name;
+      stop_json["stop_lat"] = stop.stop_lat;
+      stop_json["stop_lon"] = stop.stop_lon;
+      if (stop.parent_station.has_value()) {
+        stop_json["parent_station"] = stop.parent_station.value();
+      } else {
+        stop_json["parent_station"] = nullptr;
+      }
+      json_stops[i] = std::move(stop_json);
+    }
+
+    crow::response res(200, json_stops);
+    res.add_header("Content-Type", "application/json");
+    return res;
+  });
+
+  app.port(18080).run();
+}
