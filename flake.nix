2ccaae9d9b70fe80947efa664079ac057b254de8
Marcia Pedals <marcia.pedals@gmail.com>
2025-07-15 14:11:16 -0700

    trying to install Crow

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "C++ development environment with Crow framework";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            cmake
+            crow
+            ninja
+          ];
+        };
+      });
+}
