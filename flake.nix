cd177b7958a86421672fede731509d3c49d06ce5
Marcia Rasi <marcia.pedals@gmail.com>
2026-02-05 15:44:46 -0800

    move testdata into the cloud and fetch it with nix so that it is easier to run tests on fresh checkouts

--- flake.nix
+++ flake.nix
@@ -28,11 +28,18 @@
 
         # Concorde TSP solver
         concorde = pkgs.callPackage ./third_party/concorde.nix { inherit qsopt; };
+
+        # Test data (GTFS feeds for tests and dev tools)
+        testData = pkgs.fetchzip {
+          url = "https://pub-42e805ab0ef7400f8f4eca373798da93.r2.dev/vats5-test-data-v1.tar.gz";
+          sha256 = "sha256-9deT2h6s5qCBLukYbi7mvNhmEIfPwIxkFwPKNp0dGEk=";
+          stripRoot = false;
+        };
       in
       {
         # Export packages so they can be built with `nix build .#qsopt` etc.
         packages = {
-          inherit qsopt concorde;
+          inherit qsopt concorde testData;
           default = concorde;
         };
 
@@ -58,6 +65,12 @@
             export CC=${pkgs.clang}/bin/clang
             export CXX=${pkgs.clang}/bin/clang++
             export RC_PARAMS="max_success=5000"
+
+            # Symlink test data if server/data doesn't exist
+            if [ ! -e server/data ]; then
+              echo "Linking test data from nix store..."
+              ln -s ${testData} server/data
+            fi
           '';
         };
       });
